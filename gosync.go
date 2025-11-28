71bbf7fe3a95f31aeb7eddabbdcef285f137c089
Edward Ruzek <edadiddles@hotmail.com>
2025-11-28 08:46:56 -0500

    parsing out the args and attempting to read directories provided by input

--- gosync.go
+++ gosync.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	//"io/fs"
 	"os"
 )
 
 func main() {
-	for i := 0; i < len(os.Args); i++ {
-		fmt.Println(os.Args[i]);
+	if len(os.Args) < 3 {
+		fmt.Println("At least three arguments expected")
+		return
 	}
+
+	dir1 := os.Args[1]
+	dir2 := os.Args[2]
+
+	fmt.Println("Dir1:", dir1, "-- Dir2:", dir2)
+
+	fs1, err := os.ReadDir(dir1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fs2, err := os.ReadDir(dir2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(fs1)
+	fmt.Println(fs2)
 }
